67acaa4f52cc0f882105baa483bfe172186148a7
ndinh215 <blackjackptit@yahoo.com>
2015-03-30 15:40:40 +0700

    Fix the error mapping entity

--- src/main/java/com/panpages/bow/model/Survey.java
+++ src/main/java/com/panpages/bow/model/Survey.java
@@ -1,10 +1,37 @@
 package com.panpages.bow.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="Survey")
 public class Survey {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int survey_id;
 
+	@Size(min=3, max=50)
+	@Column(name = "NAME", nullable = false)
+	private String name;
+
+	public int getId() {
+		return survey_id;
+	}
+
+	public void setId(int id) {
+		this.survey_id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- src/main/webapp/WEB-INF/views/surveys.jsp
+++ src/main/webapp/WEB-INF/views/surveys.jsp
@@ -6,5 +6,6 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 </head>
 <body>
+	Surveys
 </body>
 </html>
