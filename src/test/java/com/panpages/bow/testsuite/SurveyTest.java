6d1f68ace1d565e9403b1e14724097a861b03ecc
ndinh215 <blackjackptit@yahoo.com>
2015-03-30 14:11:52 +0700

    Add test package

---
+++ src/test/java/com/panpages/bow/testsuite/SurveyTest.java
@@ -0,0 +1,12 @@
+package com.panpages.bow.testsuite; 
+
+import junit.framework.TestCase;
+
+public class SurveyTest extends TestCase {
+	
+	public void testFunction() throws Exception {
+		String survey = "";
+		assertEquals("Hello world!", survey);
+	}
+
+}
