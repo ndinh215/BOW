cdc06e89297a2a055cf477f9cfc4dd5b4154dc0f
ndinh215 <blackjackptit@yahoo.com>
2015-03-31 14:10:24 +0700

    Add actions proccessing routing to templates, surveys with a certain Id

--- src/main/java/com/panpages/bow/controller/AppController.java
+++ src/main/java/com/panpages/bow/controller/AppController.java
@@ -1,14 +1,15 @@
 package com.panpages.bow.controller;
 
-import java.util.List;
+import java.io.File;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.panpages.bow.model.Survey;
 import com.panpages.bow.service.ReportService;
 import com.panpages.bow.service.SurveyService;
 
@@ -16,18 +17,32 @@ import com.panpages.bow.service.SurveyService;
 @RequestMapping("/")
 public class AppController {
 
+	@Autowired ApplicationContext ctx;
+	
 	@Autowired
 	SurveyService surveySvc;
 	
 	@Autowired
 	ReportService reportSvc;
 
-	@RequestMapping(value = { "/", "/list" }, method = RequestMethod.GET)
-	public String listSurveys(ModelMap model) {
-
-		List<Survey> surveys = surveySvc.findAllSurveys();
-		model.addAttribute("surveys", surveys);
-		return "surveys";
+	@RequestMapping(value = { "/", "/index.html" }, method = RequestMethod.GET)
+	public String home(ModelMap model) {
+		return "index";
+	}
+	
+	@RequestMapping(value = { "/templates.html" }, method = RequestMethod.GET)
+	public String listTemplates(ModelMap model) {
+		return "templates";
+	}
+	
+	@RequestMapping(value = { "/survey/{surveyId}" }, method = RequestMethod.GET)
+	public String survey(@PathVariable int surveyId) {
+		String templatePath = ctx.getEnvironment().getProperty("template_path");
+		String prefixName = ctx.getEnvironment().getProperty("prefix_template_name");
+		String surveyName = String.format("%1$s%2$s", prefixName, surveyId);
+		String surveyPath = String.format("%1$s%2$s%3$s", templatePath, File.separator, surveyName);
+				
+		return surveyPath;
 	}
 	
 }
--- src/main/webapp/WEB-INF/views/templates/survey_template_1.jsp
+++ src/main/webapp/WEB-INF/views/templates/survey_template_1.jsp
@@ -6,6 +6,6 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 </head>
 <body>
-	Surveys
+	Survey template 1
 </body>
 </html>
