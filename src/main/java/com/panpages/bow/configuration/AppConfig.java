506bc792496f04e97cfca2007368bb096e996f60
ndinh215 <blackjackptit@yahoo.com>
2015-04-01 22:33:11 +0700

    Add output folder for templates filled

--- src/main/java/com/panpages/bow/configuration/AppConfig.java
+++ src/main/java/com/panpages/bow/configuration/AppConfig.java
@@ -44,6 +44,7 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/assets/**").addResourceLocations("/WEB-INF/views/assets/");
+        registry.addResourceHandler("/storage/**").addResourceLocations("/WEB-INF/storage/");
     }
 }
 
--- src/main/java/com/panpages/bow/service/ReportServiceImpl.java
+++ src/main/java/com/panpages/bow/service/ReportServiceImpl.java
@@ -1,12 +1,17 @@
 package com.panpages.bow.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 @Service("reportService")
 public class ReportServiceImpl implements ReportService {
+	@Autowired ApplicationContext ctx;
 
 	@Override
 	public String exportReport(int surveyId, String format) {
+		//String templatePath = ctx.getEnvironment().getProperty("report_template_path");
+		
 		return null;
 	}
 
