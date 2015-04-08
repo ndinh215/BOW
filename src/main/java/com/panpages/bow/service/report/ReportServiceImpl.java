9adf83888cab0e4002b034b1aba7592cb9f9e9d4
ndinh215 <blackjackptit@yahoo.com>
2015-04-08 13:21:43 +0700

    Add datasource to survey in report

--- src/main/java/com/panpages/bow/service/report/ReportServiceImpl.java
+++ src/main/java/com/panpages/bow/service/report/ReportServiceImpl.java
@@ -56,7 +56,7 @@ public class ReportServiceImpl implements ReportService {
 		
 		HashMap<String, Object> params = new HashMap<String, Object>();
 		params.put("SUBREPORT_DIR", reportTemplateDirPath);
-		params.put("REPORT_DATA_SOURCE", surveys);
+		params.put("REPORT_DATA_SOURCE", new JRBeanCollectionDataSource(surveys));
 		try {
 			JasperDesign jasperDesign = JRXmlLoader.load(reportTemplatePath);
 			JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
