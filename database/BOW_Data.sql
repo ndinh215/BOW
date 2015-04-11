40ac79a2b7f889989b777bd8818e7179e61a0fc3
yentuyet <yentuyet@panpages.com>
2015-04-11 10:58:32 +0700

    Edit database

---
+++ database/BOW_Data.sql
@@ -0,0 +1,76 @@
+-- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bow
+-- ------------------------------------------------------
+-- Server version	5.6.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `field_section_relation_in_template`
+--
+
+LOCK TABLES `field_section_relation_in_template` WRITE;
+/*!40000 ALTER TABLE `field_section_relation_in_template` DISABLE KEYS */;
+/*!40000 ALTER TABLE `field_section_relation_in_template` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `field_template`
+--
+
+LOCK TABLES `field_template` WRITE;
+/*!40000 ALTER TABLE `field_template` DISABLE KEYS */;
+/*!40000 ALTER TABLE `field_template` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `section_survey_relation_in_template`
+--
+
+LOCK TABLES `section_survey_relation_in_template` WRITE;
+/*!40000 ALTER TABLE `section_survey_relation_in_template` DISABLE KEYS */;
+/*!40000 ALTER TABLE `section_survey_relation_in_template` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `section_template`
+--
+
+LOCK TABLES `section_template` WRITE;
+/*!40000 ALTER TABLE `section_template` DISABLE KEYS */;
+/*!40000 ALTER TABLE `section_template` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `survey_template`
+--
+
+LOCK TABLES `survey_template` WRITE;
+/*!40000 ALTER TABLE `survey_template` DISABLE KEYS */;
+/*!40000 ALTER TABLE `survey_template` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'bow'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-04-11 10:51:41
